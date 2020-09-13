cdcd41b255a338cd55cf219c644f2c8feb4d4f12
Hunter Price <hunterprice4044@gmail.com>
2020-09-13 15:09:42 -0400

    added code to query user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import requests
 import difflib
 from bs4 import BeautifulSoup
+import requests
 
 class QuizletParser():
     def __init__(self, url, query):
@@ -31,7 +31,57 @@ class QuizletParser():
             print("Answer")
             print(qA[i])
 
-qp = QuizletParser("https://quizlet.com/71390125/chapter-2-flash-cards/", "1) Every financial market performs the following function:A) It determines the level of interest rates.B) It allows common stock to be traded.C) It allows loans to be made.D) It channels funds from lenders-savers to borrowers-spenders.")
+
+# reads a question from stdin and returns it as a string
+def getQuestion(prompt=""):
+    print(prompt, end='')
+
+    # read until there a line is only a new line and append to query str
+    question = ''
+    while True:
+        line = input()
+        if len(line.split()) == 0:
+            break
+        line = line.replace('\n', '')
+        question += ' ' + line
+    question = question.split(" ")
+
+    # delete any words in query less than 4 characters
+    for w in question:
+        if len(w) < 4:
+            question.remove(w)
+    question = ' '.join([str(elem) for elem in question])
+    return question
+
+
+# takes a string and queries google then returns a list of tuples with the link and question
+def getGoogleResults(query, accepted_sites):
+    # search google
+    req = requests.get('http://google.com/search?q='+query)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    a = soup.find_all('a', href=True)
+
+    # gather all of the links
+    links = []
+    for l in a:
+        links.append(l.get('href').replace("/url?q=", ''))
+
+    # parse the links for accepted sites
+    sites = []
+    for l in links:
+        for a in accepted_sites:
+            if a in l:
+                sites.append((l, query))
+
+    return sites
+
+
+
+
+sites = getGoogleResults(getQuestion(), ['quizlet'])
+
+for s in sites:
+    qp = QuizletParser(s[0], s[1])
 
 
 
